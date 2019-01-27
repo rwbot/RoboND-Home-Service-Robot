679978239fa277259c0a743b4e38a5ff97a5807b
rwbot <rwbotx@gmail.com>
2019-01-27 00:09:36 -0500

    feat: specify new gmapping parameters in launch file

--- scripts/wall_follower.sh
+++ scripts/wall_follower.sh
@@ -10,7 +10,7 @@ sleep 10
 xterm -e "  roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 5
 
-xterm -e "  export TURTLEBOT_3D_SENSOR=kinect; roslaunch turtlebot_navigation gmapping_demo.launch" &
+xterm -e "  export TURTLEBOT_3D_SENSOR=kinect; roslaunch turtlebot_navigation gmapping_demo.launch custom_gmapping_launch_file:=/home/nvidia/catkin_ws/src/launch/custom_home_service_gmapping.launch" &
 sleep 5
 
 xterm -e "  rosrun wall_follower wall_follower" 
