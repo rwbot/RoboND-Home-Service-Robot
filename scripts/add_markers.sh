2db1c385107d98a4f7e7d62c0300ca0516b121d3
rwbot <rwbotx@gmail.com>
2019-01-28 02:07:03 -0500

    edit add_markers script; add marker view torviz config

--- scripts/add_markers.sh
+++ scripts/add_markers.sh
@@ -9,7 +9,7 @@ sleep 10
 xterm -e " export TURTLEBOT_3D_SENSOR=kinect; export TURTLEBOT_GAZEBO_MAP_FILE=/home/nvidia/catkin_ws/src/worlds/kinect-map.yaml; roslaunch turtlebot_gazebo amcl_demo.launch"&
 sleep 5
 #xterm -e "  roslaunch turtlebot_navigation amcl_demo.launch map_file:=/home/nvidia/catkin_ws/src/worlds/kinect-map.yaml" &
-xterm -e "  roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+xterm -e "roslaunch add_markers view_home_service.launch" &
 sleep 5
 
 xterm -e "  rosrun add_markers add_markers" 
