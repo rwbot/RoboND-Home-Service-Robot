38b4feded47f2630bbc8e282d58c3a9e6f13dd41
rwbot <rwbotx@gmail.com>
2019-01-27 20:51:51 -0500

    feat: create and configure add_markers pkg & script

---
+++ scripts/add_markers.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#source /opt/ros/kinetic/setup.bash
+#source /home/nvidia/catkin_ws/devel/setup.bash
+export TURTLEBOT_3D_SENSOR=kinect
+export TURTLEBOT_GAZEBO_MAP_FILE=/home/nvidia/catkin_ws/src/worlds/kinect-map.yaml
+
+xterm -e "  roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/nvidia/catkin_ws/src/worlds/uda.world" &
+sleep 10
+xterm -e " export TURTLEBOT_3D_SENSOR=kinect; export TURTLEBOT_GAZEBO_MAP_FILE=/home/nvidia/catkin_ws/src/worlds/kinect-map.yaml; roslaunch turtlebot_gazebo amcl_demo.launch"&
+sleep 5
+#xterm -e "  roslaunch turtlebot_navigation amcl_demo.launch map_file:=/home/nvidia/catkin_ws/src/worlds/kinect-map.yaml" &
+xterm -e "  roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+sleep 5
+
+xterm -e "  rosrun add_markers add_markers" 
+
+
